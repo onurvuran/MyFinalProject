553b311951f91d3c918e9c104f7f7a11a1642083
ONUR <ONUR@DESKTOP-36KJSTU>
2021-01-30 12:06:44 -0900

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntitiy iplemen eden class bir veriytabanı tablosudur
+   public interface IEntity
+    {
+    }
+}
